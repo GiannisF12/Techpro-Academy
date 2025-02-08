b068629f3c9a20e1d17b77037f668b3aceccea85
GiannisF12 <44063119+GiannisF12@users.noreply.github.com>
2025-02-08 19:34:30 +0200

    Initial commit

--- Assignments/DiningPhilosophers/src/App.java
+++ Assignments/DiningPhilosophers/src/App.java
@@ -1,17 +1,20 @@
+import java.util.concurrent.Semaphore;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Philosopher[] philosophers = new Philosopher[5];
-        Object[] forks = new Object[philosophers.length];
+        Semaphore[] forks = new Semaphore[philosophers.length];
 
         for (int i = 0; i < forks.length; i++) {
-            forks[i] = new Object();
+            forks[i] = new Semaphore(1);
         }
 
         for (int i = 0; i < philosophers.length; i++) {
-            Object leftFork = forks[i];
-            Object rightFork = forks[(i + 1) % forks.length];
+            Semaphore leftFork = forks[i];
+            Semaphore rightFork = forks[(i + 1) % forks.length];
 
-            philosophers[i] = new Philosopher(leftFork, rightFork);
+            boolean isLastPhilosopher = (i == 5 - 1);
+            philosophers[i] = new Philosopher(leftFork, rightFork,isLastPhilosopher);
             
             Thread t 
               = new Thread(philosophers[i], "Philosopher " + (i + 1));
--- Assignments/DiningPhilosophers/src/Philosopher.java
+++ Assignments/DiningPhilosophers/src/Philosopher.java
@@ -1,26 +1,62 @@
+import java.util.concurrent.Semaphore;
+
 public class Philosopher implements Runnable{
 
 
-    private Object leftFork;
-    private Object rightFork;
+    private final Semaphore leftFork;
+    private final Semaphore rightFork;
+    private final Semaphore max = new Semaphore(4);
+    private final boolean isLastPhilosopher;
 
-    public Philosopher(Object leftFork, Object rightFork) {
+    public Philosopher(Semaphore leftFork, Semaphore rightFork, boolean isLastPhilosopher) {
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        this.isLastPhilosopher = isLastPhilosopher;
     }
 
 
     private void doAction(String action) throws InterruptedException {
         System.out.println(
           Thread.currentThread().getName() + " " + action);
-        Thread.sleep(((int) (Math.random() * 10)));
+        Thread.sleep(((int) (Math.random() * 100)));
     }
 
     @Override
     public void run() {
-        // Populate this method
-        while(true){
+        try {
+            while(true){
+                doAction("Thinking..");
 
+                max.acquire();
+                if(isLastPhilosopher){
+                    rightFork.acquire();
+                    doAction("picked up right fork.");
+
+                    leftFork.acquire();
+                    doAction("picked up left fork - Eating.");
+                } else {
+                    leftFork.acquire();
+                    doAction("picked up left fork.");
+
+                    rightFork.acquire();
+                    doAction("picked up right fork - Eating.");
+                }
+
+                doAction("is eating...");
+                Thread.sleep(((int) (Math.random() * 100)));
+
+                rightFork.release();
+                doAction("put down right fork.");
+
+                leftFork.release();
+                doAction("put down left fork.");
+
+                // Allow another philosopher to eat
+                max.release();
+
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 
